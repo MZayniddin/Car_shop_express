2c4708672f749bab63775b7a87cba30e60dbe62e
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-06-09 09:39:50 +0500

    added role verification to brand api

--- routes/api/brands.js
+++ routes/api/brands.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 const brandsCtr = require("../../controllers/brandsController");
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../../middleware/verifyRoles");
 
-router.route("/").get(brandsCtr.getAllBrand).post(brandsCtr.createBrand);
-router.route("/:id").put(brandsCtr.updateBrand).delete(brandsCtr.deleteBrand);
+router
+    .route("/")
+    .get(brandsCtr.getAllBrand)
+    .post(verifyRoles(ROLES_LIST.Admin), brandsCtr.createBrand);
+
+router
+    .route("/:id")
+    .put(verifyRoles(ROLES_LIST.Admin), brandsCtr.updateBrand)
+    .delete(verifyRoles(ROLES_LIST.Admin), brandsCtr.deleteBrand);
 
 module.exports = router;
--- routes/api/cars.js
+++ routes/api/cars.js
@@ -3,12 +3,16 @@ const ROLES_LIST = require("../../config/roles_list");
 const carsController = require("../../controllers/carsController");
 const verifyRoles = require("../../middleware/verifyRoles");
 
-router.route("/").get(carsController.getAllCars).post(carsController.createCar);
+router
+    .route("/")
+    .get(carsController.getAllCars)
+    .post(verifyRoles(ROLES_LIST.Admin), carsController.createCar);
+
 router
     .route("/:id")
     .put(verifyRoles(ROLES_LIST.Admin), carsController.updateCar)
     .delete(verifyRoles(ROLES_LIST.Admin), carsController.deleteCar);
 
-router.patch("/like/:id", carsController.likeCar)
+router.patch("/like/:id", carsController.likeCar);
 
 module.exports = router;
